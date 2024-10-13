7aebe405a3cb718802c78f184f9c1fd483a46c55
IvaN <113847518+Ivan-3101@users.noreply.github.com>
2024-10-13 22:29:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,14 @@ import disease
 import diet
 import packed
 import home
+
 # Move this line to the top, outside of any function
 st.set_page_config(page_title="Food Safety & Health Analyzer", page_icon="🍽️")
 
 def main():
     st.sidebar.title("Navigation")
-    page = st.sidebar.selectbox("Choose a page", ["Home", "Ingredient Analysis","Disease Prediction", "Diet Recommendation","Packed Food Analysis"])
+    page = st.sidebar.selectbox("Choose a page", ["Home", "Ingredient Analysis", "Disease Prediction", "Diet Recommendation", "Packed Food Analysis"])
 
-    # if page == "Healthy Food Analysis":
-    #     st.title("🥗 Healthy Food Analysis")
-    #     healthy.main()
     if page == "Ingredient Analysis":
         st.title("🧪 Ingredient Analysis")
         ingredients.main()
@@ -26,6 +24,9 @@ def main():
     elif page == "Packed Food Analysis":
         st.title("🔎 Packed Food Analysis")
         packed.main()
+    elif page == "Home":
+        st.title("🍽️ Food Image Classifier and Health Analyzer")
+        home.main()
     else:
         st.title("🍽️ AI-Powered Personalized Diet Recommender")
         diet.main()
